44cd2ba05d118e70e4bd1a94cc0489548a60a531
tatugrigalashvili <146704405+tatugrigalashvili@users.noreply.github.com>
2024-06-14 13:38:41 +0400

    Add files via upload

---
+++ davaleba.js
@@ -0,0 +1,11 @@
+var object = {
+    name: "Tatu",
+    lastname: "Grigalashvili",
+    age: 14,
+    height: 170 +"cm"
+};
+
+document.write(object.name + "<br>")
+document.write(object.lastname + "<br>")
+document.write(object.age + "<br>")
+document.write(object.height)
